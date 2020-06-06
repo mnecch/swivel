5beb36852c6dd86c61cbd2f1d3d9800720cabc4f
Michael Necio <mnecch@gmail.com>
2020-06-06 00:10:15 -0400

    Less verbosity

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -3,19 +3,11 @@
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  Serial.println("Started");
 
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
     while (1);
   }
-
-  Serial.print("Accelerometer sample rate = ");
-  Serial.print(IMU.accelerationSampleRate());
-  Serial.println(" Hz");
-  Serial.println();
-  Serial.println("Acceleration in G's");
-  Serial.println("X\tY\tZ");
 }
 
 void loop() {
